62484206ed2191ab3bc85fb7b7c90540dc4f6dcb
mnmpk <mnmpkm@gmail.com>
2024-10-28 23:07:35 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,121 +1,167 @@
 
-$(window).on('load', function() {
-    removeServers = function(){
+$(window).on('load', function () {
+    removeServers = function () {
         newLines = "\n";
-        newLines += "export REGIONS=("+$('#removeServersRegions').val()+")\n";
-        newLines += "export OWNER=\""+$('#removeServersOwner').val()+"\"\n";
-        newLines += "export TAGNAME=\""+$('#removeServersTagName').val()+"\"";
+        newLines += "export REGIONS=(" + $('#removeServersRegions').val() + ")\n";
+        newLines += "export OWNER=\"" + $('#removeServersOwner').val() + "\"\n";
+        newLines += "export TAGNAME=\"" + $('#removeServersTagName').val() + "\"";
         $("#removeServers code").text(replaceFirstNLines($("#removeServers code").text(), newLines));
     }
     $('#removeServers .form-control').on('change', removeServers);
 
-    securityGroup = function(){
+    append = function () {
+        let cId = this.parentElement.parentElement.id;
+        /*if ("awsEnvVar" == this.id)
+            cId = "awsEnvVar";*/
+        if ($("#final_" + cId).length) {
+            $("#final_" + cId).remove();
+        } else {
+            let value = $("#" + cId + " code").text();
+            if ("awsSetting" == cId){
+                value = $('#awsEnvVar').val();
+                //value.replaceAll("\n","<br/>");
+            }
+            $('#final').append("<pre id='final_" + cId + "'>" + value + "</pre>");
+        }
+    }
+    $('input[type=checkbox]').on('click', append);
+
+    awsSetting = function () {
+        newLines = "";
+        newLines += "export AWS_DEFAULT_REGION=" + $('#awsRegion').val() + "\n";
+        newLines += "export AWS_ACCESS_KEY_ID=\"" + $('#awsAccessKeyId').val() + "\"\n";
+        newLines += "export AWS_SECRET_ACCESS_KEY=\"" + $('#awsSecretAccessKey').val() + "\"\n";
+        newLines += "export AWS_SESSION_TOKEN=\"" + $('#awsSessionToken').val() + "\"";
+        $("#awsEnvVar").val(newLines);
+    }
+    $('#awsSetting .form-control').on('change', awsSetting);
+
+    //$('#awsEnvVar').on('change', append);
+
+    removeServers = function () {
         newLines = "\n";
-        newLines += "export REGION=\""+$('#securityGroupRegion').val()+"\"\n";
-        newLines += "export SG_NAME=\""+$('#securityGroupName').val()+"\"\n";
-        newLines += "export SG_DESCRIPTION=\""+$('#securityGroupDesc').val()+"\"\n";
+        newLines += "export REGIONS=(" + $('#removeServersRegions').val() + ")\n";
+        newLines += "export OWNER=\"" + $('#removeServersOwner').val() + "\"\n";
+        newLines += "export TAGNAME=\"" + $('#removeServersTagName').val() + "\"";
+        $("#removeServers code").text(replaceFirstNLines($("#removeServers code").text(), newLines));
+    }
+    $('#removeServers .form-control').on('change', removeServers);
+
+    securityGroup = function () {
+        newLines = "\n";
+        newLines += "export REGION=\"" + $('#securityGroupRegion').val() + "\"\n";
+        newLines += "export SG_NAME=\"" + $('#securityGroupName').val() + "\"\n";
+        newLines += "export SG_DESCRIPTION=\"" + $('#securityGroupDesc').val() + "\"\n";
         $("#securityGroup code").text(replaceFirstNLines($("#securityGroup code").text(), newLines));
     }
     $('#securityGroup .form-control').on('change', securityGroup);
-    
-    
-    launchInstances = function(){
+
+
+    launchInstances = function () {
         newLines = "\n";
-        newLines += "export REGION=\""+$('#launchInstanceRegion').val()+"\"\n";
-        newLines += "export SG_NAME=\""+$('#launchInstanceSGName').val()+"\"\n";
-        newLines += "export PG_NAME=\""+$('#launchInstancePGName').val()+"\"\n";
-        newLines += "export ITYPE=\""+$('#launchInstanceInstanceType').val()+"\"\n";
-        newLines += "export IMG_NAME=\""+$('#launchInstanceImageName').val()+"\"\n";
-        newLines += "export DISKS='"+$('#launchInstanceDisks').val()+"'\n";
-        newLines += "export KEY_NAME=\""+$('#launchInstanceKeyName').val()+"\"\n";
-        newLines += "export TAGS='"+$('#launchInstanceTags').val()+"'\n";
-        newLines += "export NO_OF_HOST="+$('#launchInstanceNoOfHosts').val();
+        newLines += "export REGION=\"" + $('#launchInstanceRegion').val() + "\"\n";
+        newLines += "export SG_NAME=\"" + $('#launchInstanceSGName').val() + "\"\n";
+        newLines += "export PG_NAME=\"" + $('#launchInstancePGName').val() + "\"\n";
+        newLines += "export ITYPE=\"" + $('#launchInstanceInstanceType').val() + "\"\n";
+        newLines += "export IMG_NAME=\"" + $('#launchInstanceImageName').val() + "\"\n";
+        newLines += "export DISKS='" + $('#launchInstanceDisks').val() + "'\n";
+        newLines += "export KEY_NAME=\"" + $('#launchInstanceKeyName').val() + "\"\n";
+        newLines += "export TAGS='" + $('#launchInstanceTags').val() + "'\n";
+        newLines += "export NO_OF_HOST=" + $('#launchInstanceNoOfHosts').val();
         $("#launchInstance code").text(replaceFirstNLines($("#launchInstance code").text(), newLines));
     }
     $('#launchInstance .form-control').on('change', launchInstances);
-    
-    dns = function(){
+
+    dns = function () {
         newLines = "\n";
-        newLines += "export DOMAIN=\""+$('#dnsDomain').val()+"\"\n";
-        newLines += "export HOSTEDZONE=\""+$('#dnsHostedZoneId').val()+"\"\n";
-        newLines += "export INTERNAL_HOSTLIST="+$('#dnsInternalHostList').val()+"\n";
-        newLines += "export INTERNAL_SUBDOMAIN=\""+$('#dnsInternalSubdomainPrefix').val()+"\"\n";
-        newLines += "export EXTERNAL_HOSTLIST="+$('#dnsExternalHostList').val()+"\n";
-        newLines += "export EXTERNAL_SUBDOMAIN=\""+$('#dnsExternalSubdomainPrefix').val()+"\"";
+        newLines += "export DOMAIN=\"" + $('#dnsDomain').val() + "\"\n";
+        newLines += "export HOSTEDZONE=\"" + $('#dnsHostedZoneId').val() + "\"\n";
+        newLines += "export INTERNAL_HOSTLIST=" + $('#dnsInternalHostList').val() + "\n";
+        newLines += "export INTERNAL_SUBDOMAIN=\"" + $('#dnsInternalSubdomainPrefix').val() + "\"\n";
+        newLines += "export EXTERNAL_HOSTLIST=" + $('#dnsExternalHostList').val() + "\n";
+        newLines += "export EXTERNAL_SUBDOMAIN=\"" + $('#dnsExternalSubdomainPrefix').val() + "\"";
         $("#dns code").text(replaceFirstNLines($("#dns code").text(), newLines));
     }
     $('#dns .form-control').on('change', dns);
 
-    prodNotes = function(){
+    prodNotes = function () {
         newLines = "\n";
-        newLines += "export HOSTLIST=("+$('#productionNotesHostList').val()+")\n";
-        newLines += "export PREFIX=\""+$('#productionNotesExternalDNSPrefix').val()+"\"\n";
-        newLines += "export KEYPATH=\""+$('#productionNotesKeyPath').val()+"\"";
+        newLines += "export HOSTLIST=(" + $('#productionNotesHostList').val() + ")\n";
+        newLines += "export PREFIX=\"" + $('#productionNotesExternalDNSPrefix').val() + "\"\n";
+        newLines += "export KEYPATH=\"" + $('#productionNotesKeyPath').val() + "\"";
         $("#productionNotes code").text(replaceFirstNLines($("#productionNotes code").text(), newLines));
     }
     $('#productionNotes .form-control').on('change', prodNotes);
 
-    automationAgents = function(){
+    automationAgents = function () {
         newLines = "\n";
-        newLines += "export HOSTLIST=("+$('#automationAgentsHostList').val()+")\n";
-        newLines += "export PREFIX=\""+$('#automationAgentsExternalDNSPrefix').val()+"\"\n";
-        newLines += "export KEYPATH=\""+$('#automationAgentsKeyPath').val()+"\"\n";
-        newLines += "export GROUPID=\""+$('#automationAgentsGroupId').val()+"\"\n";
-        newLines += "export AGENTSAPIKEY=\""+$('#automationAgentsApiKey').val()+"\"";
+        newLines += "export HOSTLIST=(" + $('#automationAgentsHostList').val() + ")\n";
+        newLines += "export PREFIX=\"" + $('#automationAgentsExternalDNSPrefix').val() + "\"\n";
+        newLines += "export KEYPATH=\"" + $('#automationAgentsKeyPath').val() + "\"\n";
+        newLines += "export GROUPID=\"" + $('#automationAgentsGroupId').val() + "\"\n";
+        newLines += "export AGENTSAPIKEY=\"" + $('#automationAgentsApiKey').val() + "\"";
         $("#automationAgents code").text(replaceFirstNLines($("#automationAgents code").text(), newLines));
     }
     $('#automationAgents .form-control').on('change', automationAgents);
 
-    configMongod = function(){
+    configMongod = function () {
         newLines = "\n";
-        newLines += "export PATH=\""+$('#configMongodPath').val()+"\"\n";
-        newLines += "export DBPATH=\"$PATH"+$('#configMongodDBPath').val()+"\"\n";
-        newLines += "export LOGPATH=\"$PATH"+$('#configMongodLogPath').val()+"\"\n";
-        newLines += "export KEYPATH=\"$PATH"+$('#configMongodKeyPath').val()+"\"\n";
+        newLines += "export PATH=\"" + $('#configMongodPath').val() + "\"\n";
+        newLines += "export DBPATH=\"$DATAPATH" + $('#configMongodDBPath').val() + "\"\n";
+        newLines += "export LOGPATH=\"$DATAPATH" + $('#configMongodLogPath').val() + "\"\n";
+        newLines += "export KEYPATH=\"$DATAPATH" + $('#configMongodKeyPath').val() + "\"\n";
+        newLines += "export KEYFILE=\"" + $('#configMongodKey').val() + "\"\n";
         $("#configMongod code").text(replaceFirstNLines($("#configMongod code").text(), newLines));
     }
     $('#configMongod .form-control').on('change', configMongod);
 
-    hosts = function(){
+    hosts = function () {
         newLines = "\n";
-        newLines += "export DOMAIN=\""+$('#hostsDomain').val()+"\"\n";
-        newLines += "export INTERNAL_SUBDOMAIN=\""+$('#hostsInternalSubdomain').val()+"\"\n";
-        newLines += "export EXTERNAL_SUBDOMAIN=\""+$('#hostsExternalSubdomain').val()+"\"";
+        newLines += "export DOMAIN=\"" + $('#hostsDomain').val() + "\"\n";
+        newLines += "export INTERNAL_SUBDOMAIN=\"" + $('#hostsInternalSubdomain').val() + "\"\n";
+        newLines += "export EXTERNAL_SUBDOMAIN=\"" + $('#hostsExternalSubdomain').val() + "\"";
         $("#hosts code").text(replaceFirstNLines($("#hosts code").text(), newLines));
     }
     $('#dns .form-control').on('change', dns);
 
-    configRS = function(){
+    configRS = function () {
         newLines = "\n";
-        newLines += "export RSNAME=\""+$('#configRSName').val()+"\"\n";
+        newLines += "export RSNAME=\"" + $('#configRSName').val() + "\"\n";
         $("#configRS code").text(replaceFirstNLines($("#configRS code").text(), newLines));
     }
     $('#configRS .form-control').on('change', configRS);
 
-    replicaSet = function(){
+    replicaSet = function () {
         newLines = "\n";
-        newLines += "mongo --eval 'rs.initiate("+$('#replicaSetDocument').val()+")'";
+        newLines += "mongo --eval 'rs.initiate(" + $('#replicaSetDocument').val() + ")'";
         $("#replicaSet code").text(newLines);
     }
     $('#replicaSet .form-control').on('change', replicaSet);
 
-    generateTLSCert = function(){
+    opsManager = function () {
         newLines = "\n";
-        newLines += "export HOSTLIST=("+$('#generateTLSCertHostList').val()+")\n";
-        newLines += "export ALTNAME=\""+$('#generateTLSCertAltName').val()+"\"\n";
-        newLines += "export C=\""+$('#generateTLSCertC').val()+"\"\n";
-        newLines += "export ST=\""+$('#generateTLSCertST').val()+"\"\n";
-        newLines += "export L=\""+$('#generateTLSCertL').val()+"\"\n";
-        newLines += "export O=\""+$('#generateTLSCertO').val()+"\"\n";
-        newLines += "export OU=\""+$('#generateTLSCertOU').val()+"\"\n";
-        newLines += "export ROOTCN=\""+$('#generateTLSCertCN').val()+"\"\n";
+        newLines += "export VERSION=\"" + $('#opsManagerVersion').val() + "\"";
+        $("#opsManager code").text(newLines);
+    }
+    $('#opsManager .form-control').on('change', replicaSet);
+
+    generateTLSCert = function () {
+        newLines = "\n";
+        newLines += "export HOSTLIST=(" + $('#generateTLSCertHostList').val() + ")\n";
+        newLines += "export ALTNAME=\"" + $('#generateTLSCertAltName').val() + "\"\n";
+        newLines += "export C=\"" + $('#generateTLSCertC').val() + "\"\n";
+        newLines += "export ST=\"" + $('#generateTLSCertST').val() + "\"\n";
+        newLines += "export L=\"" + $('#generateTLSCertL').val() + "\"\n";
+        newLines += "export O=\"" + $('#generateTLSCertO').val() + "\"\n";
+        newLines += "export OU=\"" + $('#generateTLSCertOU').val() + "\"\n";
+        newLines += "export ROOTCN=\"" + $('#generateTLSCertCN').val() + "\"\n";
         $("#generateTLSCert code").text(replaceFirstNLines($("#generateTLSCert code").text(), newLines));
     }
     $('#generateTLSCert .form-control').on('change', generateTLSCert);
 });
-function replaceFirstNLines(originalText, newLines){
+function replaceFirstNLines(originalText, newLines) {
     let lines = originalText.split('\n');
-    lines.splice(0,newLines.split("\n").length);
+    lines.splice(0, newLines.split("\n").length);
     const newArray = [newLines].concat(lines)
     return newArray.join('\n');
 }
